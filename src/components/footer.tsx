38a1f01b6efc69c2e8eef1ca47de15c26d2256c3
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-04-09 00:56:17 +0900

    update: separate 2 line, footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,9 @@ import styles from '../styles/footer.module.css'
 const Footer = () => (
   <footer className={styles.footer}>
     <div>
-      <span>©️2022 shmn7iii | Powered by </span>
+      <span>©️2022 shmn7iii</span>
+      <br />
+      <span>Powered by </span>
       <ExtLink href="https://github.com/otoyo/easy-notion-blog">
         easy-notion-blog
       </ExtLink>
