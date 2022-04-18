f7907c2da70e6bdc418612404c253a0ca6d7860f
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-04-18 22:19:05 +0900

    update: improve footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/footer.module.css'
 const Footer = () => (
   <footer className={styles.footer}>
     <div>
-      <span>©︎2022 </span>
+      <span>©︎ 2022 </span>
       <a href="https://www.shmn7iii.net"> shmn7iii </a>
       <span> | Powered by </span>
       <a href="https://github.com/shmn7iii/easy-notion-blog">
