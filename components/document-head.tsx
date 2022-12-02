c65a769e7a344ab50093452324fe1fe688fd544d
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-12-02 23:39:02 +0900

    feat: prepare to Next12
    ref. https://github.com/otoyo/easy-notion-blog/pull/138

--- components/document-head.tsx
+++ components/document-head.tsx
@@ -1,20 +1,20 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-
-import { NEXT_PUBLIC_URL } from '../lib/notion/server-constants'
-
-export const SITE_TITLE = 'shmn7iii.net'
-export const SITE_DESCRIPTION = 'shmn7iii site'
+import {
+  NEXT_PUBLIC_URL,
+  NEXT_PUBLIC_SITE_TITLE,
+  NEXT_PUBLIC_SITE_DESCRIPTION,
+} from '../lib/notion/server-constants'
 
 const DocumentHead = ({ title = '', description = '', urlOgImage = '' }) => {
   const { asPath } = useRouter()
 
   return (
     <Head>
-      <title>{title ? `${title} - ${SITE_TITLE}` : SITE_TITLE}</title>
+      <title>{title ? `${title} - ${NEXT_PUBLIC_SITE_TITLE}` : NEXT_PUBLIC_SITE_TITLE}</title>
       <meta
         name="description"
-        content={description ? description : SITE_DESCRIPTION}
+        content={description ? description : NEXT_PUBLIC_SITE_DESCRIPTION}
       />
       {NEXT_PUBLIC_URL ? (
         <meta
@@ -22,10 +22,10 @@ const DocumentHead = ({ title = '', description = '', urlOgImage = '' }) => {
           content={new URL(asPath, NEXT_PUBLIC_URL).toString()}
         />
       ) : null}
-      <meta property="og:title" content={title ? title : SITE_TITLE} />
+      <meta property="og:title" content={title ? title : NEXT_PUBLIC_SITE_TITLE} />
       <meta
         property="og:description"
-        content={description ? description : SITE_DESCRIPTION}
+        content={description ? description : NEXT_PUBLIC_SITE_DESCRIPTION}
       />
       {urlOgImage ? (
         <meta property="og:image" content={urlOgImage} />
