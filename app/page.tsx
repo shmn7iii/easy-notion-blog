5d72349679bb4f1aa1f22d4b127b9d6a72873c0e
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-01-07 19:21:58 +0900

    comment out rendering _index post

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,42 @@
 import Image from 'next/image'
-import GoogleAnalytics from '../components/google-analytics'
-import {
-  PostBody
-} from '../components/blog-parts'
+// import {
+//   PostBody
+// } from '../components/blog-parts'
 import styles from '../styles/portfolio.module.css'
-import {
-  getAllPosts,
-  getPostBySlug,
-  getAllBlocksByBlockId,
-} from '../lib/notion/client'
+// import {
+//   getAllPosts,
+//   getPostBySlug,
+//   getAllBlocksByBlockId,
+// } from '../lib/notion/client'
 
 export const revalidate = 30
 
-export async function generateStaticParams() {
-  const posts = await getAllPosts()
-  return posts.map(p => ({ slug: p.Slug }))
-}
+// export async function generateStaticParams() {
+//   const posts = await getAllPosts()
+//   return posts.map(p => ({ slug: p.Slug }))
+// }
 
 const RootPage = async () => {
-  const post = await getPostBySlug('_index')
+  // const post = await getPostBySlug('_index')
 
   // debug
-  console.log(post)
+  // console.log(post)
 
-  const [
-    blocks,
-  ] = await Promise.all([
-    getAllBlocksByBlockId(post.PageId),
-  ])
+  // const [
+  //   blocks,
+  // ] = await Promise.all([
+  //   getAllBlocksByBlockId(post.PageId),
+  // ])
 
   return (
     <>
-      <GoogleAnalytics pageTitle={post.Title} />
       <div>
         <div className={styles.icon}>
           <Image src="/icon.png" width={128} height={128} alt="icon" />
         </div>
 
         <div className={styles.post}>
-          <PostBody blocks={blocks} />
+          {/* <PostBody blocks={blocks} /> */}
         </div>
       </div>
     </>
